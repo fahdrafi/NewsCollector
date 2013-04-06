854ca88eade5edf0cc59f785e78deee9035a4936
Salman Khan <salman.khan@gmail.com>
2013-04-06 01:00:13 +0100

    Getting links from DB

--- NewsCollector/src/com/twovix/storyfetcher/StoryFetcher.scala
+++ NewsCollector/src/com/twovix/storyfetcher/StoryFetcher.scala
@@ -1,11 +1,14 @@
 package com.twovix.storyfetcher
 
 import java.net.{URLConnection, URL}
-import scala.xml.{Elem, XML}
+import scala.io.Source.{fromInputStream}
+import scala.xml.{Elem, XML, Node}
 import scala.xml.factory.XMLLoader
 import org.xml.sax._
 import org.ccil.cowan.tagsoup.jaxp.SAXFactoryImpl
 import scala.collection.mutable.Map
+import com.mongodb._;
+
  
 
 
@@ -14,7 +17,19 @@ trait SiteLoader {
   val parser = parserFactory.newSAXParser()
   val adapter = new scala.xml.parsing.NoBindingFactoryAdapter
 
+  def attributeEquals(name:String, value: String)(node: Node) = {
+    val rval = node.attributes.exists(x => (x.key == name && x.value.text == value))
+    		if(rval)
+    		  println("F: " + name + "  " + value + node)
+    rval
+  }
+  
   def loadXMLFromURLString(mURL:String):scala.xml.Node  = {
+    val url = new URL(mURL)
+    val urlCon = url.openConnection()
+    urlCon.setConnectTimeout(2000)
+    urlCon.setReadTimeout( 1000 )	
+    val content = fromInputStream( urlCon.getInputStream ).getLines.mkString("\n")
     val source = new org.xml.sax.InputSource(new URL(mURL).openStream())
     val feed = adapter.loadXML(source, parser)
     feed
@@ -49,20 +64,41 @@ object RawLoader extends SiteLoader {
 
 object BBCLoader extends SiteLoader {
   override def getStoryBody(pageXML:scala.xml.Node):scala.xml.NodeSeq = {
-    for(p <- ( pageXML \\ "div")) {
-      val divclass = p \ "@class"
-      if(divclass.text == "story-body")
-        return p
-    }
-    //if story-body not found, return entire page
-    return pageXML
+    return  (pageXML \\ "div").filter(attributeEquals("class","story-body"))
   }  
 }
 
-object TestBBC {
+object NYTLoader extends SiteLoader {
+  override def getStoryBody(pageXML:scala.xml.Node):scala.xml.NodeSeq = {
+    return  (pageXML \\ "div").filter(attributeEquals("class","articleBody"))
+  }  
+}
+object ReutersLoader extends SiteLoader {
+  override def getStoryBody(pageXML:scala.xml.Node):scala.xml.NodeSeq = {
+    return  (pageXML \\ "span" ).filter(attributeEquals("id","articleText"))
+  }    
+}
+object ScrapeLinks {
 	def main(args: Array[String]) {
-    val WordMap =  RawLoader.getWordMapFromURLString("http://www.bbc.co.uk/news/uk-scotland-22015175")
-    for(t <- WordMap)
-      println(t._1 + "\t" + t._2)
+	val mongoClient = new MongoClient("localhost")
+	val db = mongoClient.getDB("newsdb")
+	val links = db.getCollection("newslinks")
+	val linkCursor = links.find()
+	
+	while(linkCursor.hasNext()) {
+		val linkObject = linkCursor.next()
+		val link = linkObject.get("link").toString()
+		println(link)
+		try{
+		  val WordMap =  RawLoader.getWordMapFromURLString(link)
+		} catch {
+		  case e:java.net.SocketTimeoutException => println("Connection timed out")
+		}
+	}
+    //val WordMap =  ReutersLoader.getWordMapFromURLString("http://uk.reuters.com/article/2013/04/05/uk-korea-north-idUKBRE93407U20130405")
+    //val sortedFreq = WordMap.toSeq.sortBy(_._1)
+    //for(t <- sortedFreq)
+    //  println(t._1 + "\t" + t._2)
+    //  println("All done")
   }
 }
